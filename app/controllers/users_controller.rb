5499f07b8435d876384602974235d51c3ba5f491
crullian <crullian@gmail.com>
2014-03-15 15:52:21 -0400

    Fix bugs in current_user assignment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   end
 
   def create
-  	@user = User.new(params[:user])
+  	@user = User.new(params[:user]) 
   	if @user.save
       sign_in @user
       flash[:success] = "Welcome to the Sample App!"
